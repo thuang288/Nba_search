b60efc33ea4406d97fe7ebb184876004f18c1378
Tommy <thuang20018@yahoo.com>
2021-02-11 16:27:26 -0500

    Updated Settings

--- nba_search/settings.py
+++ nba_search/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ph426j_6wv3q5s^x8w57)zol*6wxt-x88+arv-_pc)1=twwby)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nbaplayersearch.herokuapp.com']
 
 
 # Application definition
